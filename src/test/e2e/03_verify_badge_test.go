8fb3dddecdd65cad90def2dd1c477474a81f1759
Jordan McClintock <jordan@defenseunicorns.com>
2025-03-10 20:04:33 +0000

    fix: tests broken from zarf changing how the message library works

--- src/test/e2e/03_verify_badge_test.go
+++ src/test/e2e/03_verify_badge_test.go
@@ -12,22 +12,22 @@ import (
 func TestVerifyBadgeGood(t *testing.T) {
 	stdout, stderr, err := e2e.UDSPK("verify-badge", "-d", "src/test/verify/mattermost-good")
 	require.NoError(t, err, stdout, stderr)
-	require.Contains(t, stderr, "Namespaces (1): [mattermost]")
+	require.Contains(t, stdout, "Namespaces (1): [mattermost]")
 }
 
 func TestVerifyBadgeWarnings(t *testing.T) {
 	stdout, stderr, err := e2e.UDSPK("verify-badge", "-d", "src/test/verify/mattermost-warnings")
 	require.NoError(t, err, stdout, stderr)
-	require.Contains(t, stderr, "Namespaces (1): [mattermost]")
-	require.Contains(t, stderr, "Manifests present in src/test/verify/mattermost-warnings/common/zarf.yaml")
-	require.Contains(t, stderr, "Manifests present in src/test/verify/mattermost-warnings/zarf.yaml")
+	require.Contains(t, stdout, "Namespaces (1): [mattermost]")
+	require.Contains(t, stdout, "Manifests present in src/test/verify/mattermost-warnings/common/zarf.yaml")
+	require.Contains(t, stdout, "Manifests present in src/test/verify/mattermost-warnings/zarf.yaml")
 }
 
 func TestVerifyBadgeBad(t *testing.T) {
 	stdout, stderr, err := e2e.UDSPK("verify-badge", "-d", "src/test/verify/mattermost-errors-and-warnings")
 	require.Error(t, err, stdout, stderr)
-	require.Contains(t, stderr, "Namespaces (1): [mattermost]")
-	require.Contains(t, stderr, "Manifests present in src/test/verify/mattermost-errors-and-warnings/common/zarf.yaml")
-	require.Contains(t, stderr, "Manifests present in src/test/verify/mattermost-errors-and-warnings/zarf.yaml")
-	require.Contains(t, stderr, "No flavors defined in in src/test/verify/mattermost-errors-and-warnings/zarf.yaml")
+	require.Contains(t, stdout, "Namespaces (1): [mattermost]")
+	require.Contains(t, stdout, "Manifests present in src/test/verify/mattermost-errors-and-warnings/common/zarf.yaml")
+	require.Contains(t, stdout, "Manifests present in src/test/verify/mattermost-errors-and-warnings/zarf.yaml")
+	require.Contains(t, stdout, "No flavors defined in in src/test/verify/mattermost-errors-and-warnings/zarf.yaml")
 }
