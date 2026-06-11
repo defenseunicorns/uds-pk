82f12132b28f313b640f7a760c746f4b921e1a11
codyshoffner <cody.shoffner@defenseunicorns.com>
2026-06-11 11:28:28 -0500

    feat: uds-pk version flags

---
+++ src/cmd/version.go
@@ -0,0 +1,25 @@
+// Copyright 2024 Defense Unicorns
+// SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// CLIVersion is set at build time via ldflags.
+var CLIVersion = "dev"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version of uds-pk",
+	Run: func(_ *cobra.Command, _ []string) {
+		fmt.Println(CLIVersion)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
