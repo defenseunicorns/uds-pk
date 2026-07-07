3a40f7de922fa30263aca7d51e163390f5f9ad47
codyshoffner <cody.shoffner@defenseunicorns.com>
2026-07-07 12:38:44 -0500

    chore: switch to path.join

--- src/cmd/release.go
+++ src/cmd/release.go
@@ -141,9 +141,9 @@ func (options *CheckOptions) run(cmd *cobra.Command, args []string) error {
 
 			var repositoryUrl string
 			if flavor == "unicorn" {
-				repositoryUrl = path.Join(baseRepo, "private", options.team, zarfPackageName)
+				repositoryUrl = baseRepo + "/" + path.Join("private", options.team, zarfPackageName)
 			} else {
-				repositoryUrl = path.Join(baseRepo, options.team, zarfPackageName)
+				repositoryUrl = baseRepo + "/" + path.Join(options.team, zarfPackageName)
 			}
 
 			log.Debug("Determined target repository", slog.String("repository", repositoryUrl))
